68b9cd3a7438f425d50579a3adcaa9263d1b38ae
keizo suzuki <keizo.suzuki3@gmail.com>
2017-01-20 11:52:47 +0900

    Initial Commit

---
+++ remocon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  remocon
+//
+//  Created by 鈴木敬三 on 2017/01/20.
+//  Copyright © 2017年 鈴木敬三. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
